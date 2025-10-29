394de01cb785308672e2b638787676fa64460295
Romaleso261981 <ladiginscormag@gmail.com>
2025-10-29 09:50:37 +0200

    Refactor: Remove server and controller files, update Dockerfile for development, and enhance .gitignore to include new directories

--- Dockerfile
+++ Dockerfile
@@ -3,12 +3,11 @@ FROM node:18-alpine
 WORKDIR /app
 
 COPY package*.json ./
-RUN npm install --only=production
+RUN npm install
 
 COPY . .
 
-ENV NODE_ENV=production
+RUN npm run build
+
 EXPOSE 5000
 CMD ["npm", "start"]
-
-
--- routes/brands.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import productsController from '../controllers/productsController.js';
-
-const router = express.Router();
-
-router.get('/', productsController.getAllBrands);
-
-export default router;
-
